543fe604648ed313f1e3af3de17472a951b8b78a
Sibusiso Matsini <Sibusiso.Matsini@altron.com>
2019-12-02 16:21:57 +0200

    S8#161 adding the build control component

---
+++ src/components/Burger/BuildControlCollection/BuildControlCollection.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './BuildControlCollection..module.css';
+
+const buildControlCollection = (props) => {
+    <div>
+
+    </div>
+}
+
+export default buildControlCollection;
