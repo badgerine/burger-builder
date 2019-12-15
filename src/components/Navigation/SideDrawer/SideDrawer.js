0ef069f689f26a5f7ee1aa8f3228243e93473438
Sibusiso Matsini <Sibusiso.Matsini@altron.com>
2019-12-15 19:54:06 +0200

    fix sideBar logo size

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,17 +2,23 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItemCollection from '../NavigationItemCollection/NavigationItemCollection';
 import classes from './SideDrawer.module.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Auxilliary from '../../../hoc/Auxilliary'
 
 const sideDrawer = (props) => (
-    <div className={classes.SideDrawer}>
-        <div className={classes.Logo}>
-            <Logo />
+    <Auxilliary >
+        <Backdrop />{/*show={props.show} clicked={props.modalClosed}/>*/}
+        <div className={classes.SideDrawer}>
+            <div className={classes.Logo}>
+            {/* <div> */}
+                <Logo />
+            </div>
+            <nav>
+                <NavigationItemCollection />
+            </nav>
         </div>
 
-        <nav>
-            <NavigationItemCollection />
-        </nav>
-    </div>
+    </Auxilliary>
 );
 
 export default sideDrawer;
