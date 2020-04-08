cecaf4b379a8819b0cb2e41614ff4318a7a19412
sibusiso matsini <sbu.matsini@gmail.com>
2020-04-08 22:59:36 +0200

    S21#221 building the checkout container

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -37,8 +37,8 @@ class BurgerBuilder extends Component {
 
     updatePurchaseState = (ingredients) => {
         // const ingredients = {...this.state.ingredients};
-        const sum = Object.keys(ingredients).
-            map(igKey => {
+        const sum = Object.keys(ingredients)
+            .map(igKey => {
                 return ingredients[igKey];
             })
             .reduce((sum, el) => {
