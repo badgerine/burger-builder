f61d993a44f64701fe474e01304e135ac3d3c191
Sibusiso Matsini <Sibusiso.Matsini@altron.com>
2020-04-14 11:28:21 +0200

    S12#228 Adding an Orders Page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Layout from './containers/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import { Switch, Route } from 'react-router-dom';
+import Orders from './containers/Orders/Orders';
 
 function App() {
   return (
     <Layout>
       <Switch>
         <Route path="/checkout" component={Checkout} />
+        <Route path="/orders" component={Orders}/>
         <Route path="/" component={BurgerBuilder} />
       </Switch>
     </Layout>
