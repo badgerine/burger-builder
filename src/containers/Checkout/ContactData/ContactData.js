4c5a5170873cc28780e62931e804170cd0fb4ff3
Sibusiso Matsini <Sibusiso.Matsini@altron.com>
2020-04-21 20:09:04 +0200

    S12#243 fixing a common validation gotcha

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -113,18 +113,18 @@ class ContactData extends Component {
     }
 
     checkValidity(value, rules) {
-        let isValid = false;
+        let isValid = true;
 
-        if (rules.required) {
-            isValid = value.trim() !== '';
+        if (rules && rules.required) {
+            isValid = isValid && value.trim() !== '';
         }
 
         if(rules.minLength) {
-            isValid = value.length >= rules.minLength;
+            isValid = isValid && value.length >= rules.minLength;
         }
 
         if(rules.maxLength) {
-            isValid = value.length <= rules.maxLength;
+            isValid = isValid && value.length <= rules.maxLength;
         }
 
         return isValid;
