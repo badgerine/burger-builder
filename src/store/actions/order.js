418526ce4a54e078b719580ce6b62d0c4e5d5ac7
sibusiso matsini <sbu.matsini@gmail.com>
2020-07-03 06:11:57 +0200

    S17#316 fetching orders (via redux)

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -28,7 +28,7 @@ export const purchaseBurger = (orderData) => {
         thunkDispatch(purchasBurgerStart);
         axios.post('orders.json/', orderData)
             .then(response => {
-                console.log('[store/reducer/orders.purchaseBurgerStart]',response);
+                console.log('[store/reducer/orders.purchaseBurgerStart]', response);
                 thunkDispatch(purchaseBurgerSuccess(response.data.name, orderData))
             })
             .catch(error => {
@@ -40,5 +40,46 @@ export const purchaseBurger = (orderData) => {
 export const purchaseInit = () => {
     return {
         type: actionTypes.PURCHASE_INIT
+    }
+}
+
+export const fetchOrdersSuccess = (orders) => {
+    return {
+        type: actionTypes.FETCH_ORDERS_SUCCESS,
+        orders: orders
+    }
+}
+
+export const fetchOrdersFail = (error) => {
+    return {
+        type: actionTypes.FETCH_ORDERS_FAIL,
+        error: error
+    }
+}
+
+export const fetchOrdersStart = () => {
+    return {
+        type: actionTypes.FETCH_ORDERS_START
+    }
+}
+
+export const fetchOrders = () => {
+    console.log('fetching orders...');
+    return thunkDispatch => {
+        thunkDispatch(fetchOrdersStart());
+        axios.get('/orders.json')
+            .then(res => {
+                const fetchedOrders = [];
+                for (let key in res.data) {
+                    fetchedOrders.push({
+                        ...res.data[key],
+                        id: key
+                    });
+                }
+                console.log('orders fetched successfully.')
+                thunkDispatch(fetchOrdersSuccess(fetchedOrders));
+            }).catch(err => {
+                thunkDispatch(fetchOrdersFail(err));
+            });
     }
 }
