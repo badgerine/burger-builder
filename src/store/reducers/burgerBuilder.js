071de77f5dd576c703df3fbd67f53978f390b27a
sibusiso matsini <sbu.matsini@gmail.com>
2020-07-05 07:30:44 +0200

    S17#318 refactoring reducers

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -1,5 +1,6 @@
 import * as actionTypes from '../actions/actionTypes';
 import * as ingredientPrices from '../../constants/IngredientItems';
+import { updateObject } from '../utility';
 
 const initialstate = {
     ingredients: null,
@@ -31,26 +32,23 @@ const updateIngredient = ingredientId => {
 const reducer = (state = initialstate, action) => {
     switch (action.type) {
         case actionTypes.ADD_INGREDIENT:
-            return {
-                ...state,
-                ingredients: {
-                    ...state.ingredients,
-                    [action.ingredientId]: state.ingredients[action.ingredientId] + 1
-                },
+            const updatedIngredient = { [action.ingredientId]: state.ingredients[action.ingredientId] + 1 };
+            const updatedIngredients = updateObject(state.ingredients, updatedIngredient);
+            const updatedState = {
+                ingredients: updatedIngredients,
                 totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientId]
-            };
+            }
+            return updateObject(state, updatedState);
         case actionTypes.REMOVE_INGREDIENT:
-            return {
-                ...state,
-                ingredients: {
-                    ...state.ingredients,
-                    [action.ingredientId]: state.ingredients[action.ingredientId] - 1
-                },
-                totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientId]
-            };
+            const updatedIng = { [action.ingredientId]: state.ingredients[action.ingredientId] - 1 };
+            const updatedIngs = updateObject(state.ingredients, updatedIng);
+            const updatedSt = {
+                ingredients: updatedIngs,
+                totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientId]
+            }
+            return updateObject(state, updatedState);
         case actionTypes.SET_INGREDIENTS:
-            return {
-                ...state,
+            const resetState = {
                 ingredients: {
                     salad: action.ingredients.salad,
                     macon: action.ingredients.macon,
@@ -60,11 +58,9 @@ const reducer = (state = initialstate, action) => {
                 totalPrice: 4,
                 error: false
             };
+            return updateObject(state, resetState);
         case actionTypes.FETCH_INGREDIENTS_FAILED:
-            return {
-                ...state,
-                error: true
-            };
+            return updateObject(state, { error: true });
         default:
             return state;
     }
