1a3bdf98fd83a5a8efee93fe0def13d74ae9ff34
sibusiso matsini <sbu.matsini@gmail.com>
2020-07-07 05:01:47 +0200

    S18#327 getting a token from the backend

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import classes from './Auth.module.css';
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
+import * as actions from '../../store/actions/index';
+import {connect} from 'react-redux';
 
 class Auth extends Component {
     state = {
@@ -77,6 +79,11 @@ class Auth extends Component {
         this.setState({ controls: updatedControls });
     }
 
+    submitHandler = (event) => {
+        event.preventDefault();
+        this.props.onAuth(this.state.controls.email, this.state.controls.password);
+    }
+
 
     render() {
         const formElementsArray = [];
@@ -101,7 +108,7 @@ class Auth extends Component {
         
         return (
             <div className={classes.Auth}>
-                <form>
+                <form onSubmit={this.submitHandler}>
                     {form}
                     <Button btnType="Success">SUBMIT</Button>
                 </form>
@@ -110,4 +117,10 @@ class Auth extends Component {
     }
 }
 
-export default Auth;
+const mapDispatchToProps = dispatch => {
+    return { 
+        onAuth: (email,password) => dispatch(actions.auth(email,password))
+    }
+}
+
+export default connect(null, mapDispatchToProps)(Auth);
