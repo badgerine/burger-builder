7a7aec5e994bf9507581a36f28a059cbf75a6c44
sibusiso matsini <sbu.matsini@gmail.com>
2020-07-13 05:25:10 +0200

    S18#339 guarding routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Layout from './containers/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
@@ -10,23 +10,43 @@ import Logout from './containers/Auth/Logout';
 import { authCheckState } from './store/actions';
 
 class App extends Component {
-  
-  componentDidMount(){
+
+  componentDidMount() {
     this.props.onTryAutoSignIn();
   }
 
   render() {
-    return (
-      <Layout>
+    let routes = (
+      <Switch>
+        <Route path="/auth" component={Auth} />
+        <Route path="/" component={BurgerBuilder} />
+        {/* <Redirect to="/" /> ###redirnced any other route */}
+      </Switch>
+    );
+
+    if (this.props.isAuthenticated) {
+      routes = (
         <Switch>
           <Route path="/checkout" component={Checkout} />
           <Route path="/orders" component={Orders} />
-          <Route path="/auth" component={Auth} />
           <Route path="/logout" component={Logout} />
           <Route path="/" component={BurgerBuilder} />
+           {/* <Redirect to="/" /> ###redirnced any other route */}
         </Switch>
+      );
+    }
+
+    return (
+      <Layout>
+        {routes}
       </Layout>
     );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
   }
 }
 
@@ -36,4 +56,4 @@ const mapDispatchToProps = dispatch => {
   }
 }
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
