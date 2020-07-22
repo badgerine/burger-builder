d8f06dc26e8159c4673e2801f922208a23bb0482
sibusiso matsini <sbu.matsini@gmail.com>
2020-07-22 07:19:26 +0200

    S27# outstanding class components for conversion

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import classes from './Auth.module.css';
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
@@ -8,121 +8,115 @@ import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { updateObject, checkValidity } from '../../utility/utility'
 
-class Auth extends Component {
-    state = {
-        controls: {
-            email: {
-                elementType: 'input',
-                elementConfig: {
-                    type: 'email',
-                    placeholder: 'Email Address'
-                },
-                value: '',
-                validation: {
-                    required: true,
-                    isEmail: true
-                },
-                valid: false,
-                touched: false
+const Auth = props => {
+    const [controls, setControls] = useState({
+        email: {
+            elementType: 'input',
+            elementConfig: {
+                type: 'email',
+                placeholder: 'Email Address'
             },
-            password: {
-                elementType: 'input',
-                elementConfig: {
-                    type: 'password',
-                    placeholder: 'Password'
-                },
-                value: '',
-                validation: {
-                    required: true,
-                    minLength: 6
-                },
-                valid: false,
-                touched: false
-            }
+            value: '',
+            validation: {
+                required: true,
+                isEmail: true
+            },
+            valid: false,
+            touched: false
         },
-        isSignUp: false
-    }
+        password: {
+            elementType: 'input',
+            elementConfig: {
+                type: 'password',
+                placeholder: 'Password'
+            },
+            value: '',
+            validation: {
+                required: true,
+                minLength: 6
+            },
+            valid: false,
+            touched: false
+        }
+    });
 
-    inputChangedHandler = (event, key) => {
+    const [isSignUp, setSignUp] = useState(false);
+
+    const inputChangedHandler = (event, key) => {
         const eventValue = event.target.value;
         //dont just shallow-clone the elements of orderForm, but deep-clone (ie clone the children too)
-        const updatedControl = updateObject(this.state.controls[key], {
+        const updatedControl = updateObject(controls[key], {
             value: eventValue,
-            valid: checkValidity(eventValue, this.state.controls[key].validation),
+            valid: checkValidity(eventValue, controls[key].validation),
             touched: true
         });
 
-        const updatedControls = updateObject(this.state.controls, {
+        const updatedControls = updateObject(controls, {
             [key]: updatedControl
         });
 
-        this.setState({ controls: updatedControls });
+        setControls(updatedControls);
     }
 
-    submitHandler = (event) => {
+    const submitHandler = (event) => {
         event.preventDefault();
-        this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value, this.state.isSignUp);
+        props.onAuth(controls.email.value, controls.password.value, isSignUp);
     }
 
-    switchAuthModeHandler = () => {
-        this.setState(prevState => {
-            return { isSignUp: !prevState.isSignUp };
-        });
+    const switchAuthModeHandler = () => {
+        setSignUp(!isSignUp);
     }
 
-
-    render() {
-        const formElementsArray = [];
-        for (let key in this.state.controls) {
-            formElementsArray.push({
-                id: key,
-                config: this.state.controls[key]
-            })
-        }
-
-        let form = formElementsArray.map(formElement => (
-            <Input
-                key={formElement.id}
-                inputType={formElement.config.elementType}
-                elementConfig={formElement.config.elementConfig}
-                value={formElement.config.value}
-                invalid={!formElement.config.valid}
-                shouldValidate={formElement.config.validation}
-                touched={formElement.config.touched}
-                changed={(event) => this.inputChangedHandler(event, formElement.id)} />
-        ));
-
-        if (this.props.loading) {
-            form = <Spinner />
-        }
-
-        let errorMessage = null;
-        if (this.props.error) {
-            errorMessage = (<p>{this.props.error.message}</p>);
-        }
-
-        //implemented simpler/more suitable solution instead of 'S18#336 redirecting the user to the checkout page'
-        let authRedirect = null;
-        if (this.props.isAuthenticated && this.props.building) {
-            authRedirect = <Redirect to="/checkout" />
-        } else if (this.props.isAuthenticated) {
-            authRedirect = <Redirect to="/" />
-        }
-
-        return (
-            <div className={classes.Auth}>
-                {authRedirect}
-                <form onSubmit={this.submitHandler}>
-                    {form}
-                    {errorMessage}
-                    <Button btnType="Success">{this.state.isSignUp ? 'SIGNUP' : 'SIGNIN'}</Button>
-                </form>
-                <Button btnType='Danger' click={this.switchAuthModeHandler}>
-                    SWITCH TO {this.state.isSignUp ? 'SIGNIN' : 'SIGNUP'}
-                </Button>
-            </div>
-        );
+    const formElementsArray = [];
+    for (let key in controls) {
+        formElementsArray.push({
+            id: key,
+            config: controls[key]
+        })
     }
+
+    let form = formElementsArray.map(formElement => (
+        <Input
+            key={formElement.id}
+            inputType={formElement.config.elementType}
+            elementConfig={formElement.config.elementConfig}
+            value={formElement.config.value}
+            invalid={!formElement.config.valid}
+            shouldValidate={formElement.config.validation}
+            touched={formElement.config.touched}
+            changed={(event) => inputChangedHandler(event, formElement.id)} />
+    ));
+
+    if (props.loading) {
+        form = <Spinner />
+    }
+
+    let errorMessage = null;
+    if (props.error) {
+        errorMessage = (<p>{props.error.message}</p>);
+    }
+
+    //implemented simpler/more suitable solution instead of 'S18#336 redirecting the user to the checkout page'
+    let authRedirect = null;
+    if (props.isAuthenticated && props.building) {
+        authRedirect = <Redirect to="/checkout" />
+    } else if (props.isAuthenticated) {
+        authRedirect = <Redirect to="/" />
+    }
+
+    return (
+        <div className={classes.Auth}>
+            {authRedirect}
+            <form onSubmit={submitHandler}>
+                {form}
+                {errorMessage}
+                <Button btnType="Success">{isSignUp ? 'SIGNUP' : 'SIGNIN'}</Button>
+            </form>
+            <Button btnType='Danger' click={switchAuthModeHandler}>
+                SWITCH TO {isSignUp ? 'SIGNIN' : 'SIGNUP'}
+            </Button>
+        </div>
+    );
 }
 
 const mapStateToProps = state => {
